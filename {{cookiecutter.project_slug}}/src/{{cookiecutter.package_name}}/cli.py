d7b8aa1e2eff99f11c9f618fb810ac7d1e046dca
zhangxianbing1 <zhangxianbing1@senseauto.com>
2022-03-15 11:07:47 +0800

    feat: add flake8 & improve cli.py

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/cli.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/cli.py
@@ -1,12 +1,14 @@
+{% import "context.j2" as ctx with context -%}
+
 """Console script for {{cookiecutter.package_name}}."""
 
-{% if cookiecutter.command_line_interface|lower == 'click' -%}
+{% if ctx.use_click -%}
 import os
 import sys
 
 import click
 
-from {{cookiecutter.package_name}} import __version__
+from . import __version__
 
 
 def version_msg():
@@ -28,4 +30,38 @@ def main():
 
 if __name__ == "__main__":
     main()  # pragma: no cover
-{%- endif %}
+
+{% elif ctx.use_typer -%}
+
+import os
+import sys
+from typing import Optional
+
+import typer
+
+from . import __name__, __version__
+
+
+def version_callback(value: bool) -> None:
+    if value:
+        python_version = sys.version[:3]
+        location = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        typer.echo(f"{__name__} {__version__} from {location} (python {python_version})")
+        raise typer.Exit()
+
+
+def cli(
+    name: str = typer.Option("World"),
+    version: Optional[bool] = typer.Option(None, "--version", callback=version_callback),
+) -> None:
+    typer.echo(f"Hello {name}")
+
+
+def main() -> None:
+    typer.run(cli)
+
+
+if __name__ == "__main__":
+    main()
+
+{% endif -%}
